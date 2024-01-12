7cba683c4aa61a22b2bfda85492d3c0b51c4858e
Noah Sehman <noah.sehman@gmail.com>
2024-01-12 11:26:56 -0700

    deleted me being mean to myself

--- htf.py
+++ htf.py
@@ -4,6 +4,11 @@ from __future__ import print_function
 import tensorflow as tf
 
 
+#please just lemme commit
+def main():
+    print("This function Does nothing")
+
 msg = tf.string_join(["Hello" , "TensorFlow"])
 with tf.Session() as sess:
     print(sess.run(msg))
+
